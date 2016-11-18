013378097c13df854a8536226780f82b4fac053c
Matthew Rocklin <mrocklin@gmail.com>
2016-11-18 16:45:19 -0500

    Update travis.yml (#1)

--- dask_glm/gradient.py
+++ dask_glm/gradient.py
@@ -1,8 +1,10 @@
-# Constants
+from __future__ import absolute_import, division, print_function
 
 import numpy as np
 import dask.array as da
 
+
+# Constants
 
 firstBacktrackMult = 0.1
 nextBacktrackMult = 0.5
@@ -38,7 +40,8 @@ def gradient(X, y, max_steps=100):
         steplen = (gradient**2).sum()**0.5
         Xgradient = X.dot(gradient)
 
-        Xbeta, eXbeta, func, gradient, steplen, Xgradient = da.compute(Xbeta, eXbeta, func, gradient, steplen, Xgradient)
+        Xbeta, eXbeta, func, gradient, steplen, Xgradient = da.compute(
+                Xbeta, eXbeta, func, gradient, steplen, Xgradient)
 
         obeta = beta
         oXbeta = Xbeta
@@ -64,7 +67,7 @@ def gradient(X, y, max_steps=100):
             break
         df /= max(func, lf)
         db = stepSize * steplen / (np.linalg.norm(beta) + stepSize * steplen)
-        print('%2d  %.6e %9.2e  %.2e  %.1e'%(k+1,func,df,db,stepSize))
+        print('%2d  %.6e %9.2e  %.2e  %.1e' % (k + 1, func, df, db, stepSize))
         if df < 1e-14:
             print('Converged')
             break
@@ -72,4 +75,3 @@ def gradient(X, y, max_steps=100):
         backtrackMult = nextBacktrackMult
 
     return beta
-
--- dask_glm/tests/test_gradient.py
+++ dask_glm/tests/test_gradient.py
@@ -1,3 +1,4 @@
+from __future__ import absolute_import, division, print_function
 
 import math
 
@@ -5,30 +6,30 @@ import dask.array as da
 import numpy as np
 import pytest
 
-from dask_glm import gradient
+from dask_glm.gradient import gradient
 
 
 def logit(y):
-    return 1.0 / ( 1.0 + da.exp(-y) )
+    return 1.0 / (1.0 + da.exp(-y))
 
 
 M = 100
 N = 100000
 S = 2
 
-X      = np.random.randn(N,M)
-X[:,1] = 1.0
-beta0  = np.random.randn(M)
+X = np.random.randn(N, M)
+X[:, 1] = 1.0
+beta0 = np.random.randn(M)
 
 
 def make_y(X, beta0=beta0):
-    N, M   = X.shape
-    z0     = X.dot(beta0)
-    z0     = da.compute(z0)[0]  # ensure z0 is a numpy array
-    scl    = S / z0.std()
+    N, M = X.shape
+    z0 = X.dot(beta0)
+    z0 = da.compute(z0)[0]  # ensure z0 is a numpy array
+    scl = S / z0.std()
     beta0 *= scl
-    z0    *= scl
-    y      = np.random.rand(N) < logit(z0)
+    z0 *= scl
+    y = np.random.rand(N) < logit(z0)
     return y, z0
 
 
@@ -42,4 +43,4 @@ dy = da.from_array(y, chunks=(N / 10,))
 
 @pytest.mark.parametrize('X,y', [(X, y), (dX, dy)])
 def test_gradient(X, y):
-    beta = gradient(X, y)
+    gradient(X, y)
