49549249eae187e1d80b7b961610d0cc9a1f56a3
Matthew Rocklin <mrocklin@gmail.com>
2016-11-18 16:41:11 -0500

    fix setup.py

--- setup.py
+++ setup.py
@@ -2,19 +2,17 @@
 
 from os.path import exists
 from setuptools import setup
-import versioneer
 
 
 setup(name='dask-glm',
-      version=versioneer.get_version(),
-      cmdclass=versioneer.get_cmdclass(),
+      version='0.0.1',
       description='Generalized Linear Models with Dask',
       url='http://github.com/dask/dask-glm/',
       maintainer='Matthew Rocklin',
       maintainer_email='mrocklin@gmail.com',
       license='BSD',
       keywords='dask,glm',
-      packages=['dask_glm']
+      packages=['dask_glm'],
       long_description=(open('README.rst').read() if exists('README.rst')
                         else ''),
       install_requires=list(open('requirements.txt').read().strip().split('\n')),
