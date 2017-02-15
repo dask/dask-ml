ecd15bb310fe4e827c33a985c4c4ec169549d3df
Jim Crist <crist042@umn.edu>
2017-02-15 15:22:52 -0600

    Add support for multiple runs
    Avoid re-tokenizing things.

--- dklearn/base.py
+++ dklearn/base.py
@@ -61,47 +61,49 @@ def fit_transform(est, X, y, fit_params):
 # -------------- #
 
 
-def do_fit(dsk, est, X, y, fit_params):
+def do_fit(dsk, est, X, y, n_splits, fit_params):
     if isinstance(est, Pipeline):
         func = do_fit_pipeline
     elif isinstance(est, FeatureUnion):
         func = do_fit_feature_union
     else:
         func = do_fit_estimator
-    return func(dsk, est, X, y, fit_params)
+    return func(dsk, est, X, y, n_splits, fit_params)
 
 
-def do_fit_transform(dsk, est, X, y, fit_params):
+def do_fit_transform(dsk, est, X, y, n_splits, fit_params):
     if isinstance(est, Pipeline):
         func = do_fit_transform_pipeline
     elif isinstance(est, FeatureUnion):
         func = do_fit_transform_feature_union
     else:
         func = do_fit_transform_estimator
-    return func(dsk, est, X, y, fit_params)
+    return func(dsk, est, X, y, n_splits, fit_params)
 
 
 # --------- #
 # Estimator #
 # --------- #
 
-def do_fit_estimator(dsk, est, X, y, fit_params):
+def do_fit_estimator(dsk, est, X, y, n_splits, fit_params):
     token = tokenize(est, X, y, fit_params)
     est_name = type(est).__name__.lower()
     name = '%s-fit-%s' % (est_name, token)
-    dsk[name] = (fit, est, X, y, fit_params)
+    for n in range(n_splits):
+        dsk[(name, n)] = (fit, est, (X, n), (y, n), fit_params)
     return name
 
 
-def do_fit_transform_estimator(dsk, est, X, y, fit_params):
+def do_fit_transform_estimator(dsk, est, X, y, n_splits, fit_params):
     token = tokenize(est, X, y, fit_params)
     name = type(est).__name__.lower()
     fit_tr_name = '%s-fit-transform-%s' % (name, token)
     fit_name = '%s-fit-%s' % (name, token)
     tr_name = '%s-transform-%s' % (name, token)
-    dsk[fit_tr_name] = (fit_transform, est, X, y, fit_params)
-    dsk[fit_name] = (getitem, fit_tr_name, 0)
-    dsk[tr_name] = (getitem, fit_tr_name, 1)
+    for n in range(n_splits):
+        dsk[(fit_tr_name, n)] = (fit_transform, est, (X, n), (y, n), fit_params)
+        dsk[(fit_name, n)] = (getitem, (fit_tr_name, n), 0)
+        dsk[(tr_name, n)] = (getitem, (fit_tr_name, n), 1)
     return fit_name, tr_name
 
 
@@ -117,51 +119,58 @@ def _group_fit_params(steps, fit_params):
     return param_lk
 
 
-def _fit_transform_steps(dsk, steps, Xt, y, param_lk):
+def _fit_transform_steps(dsk, steps, Xt, y, n_splits, param_lk):
     fits = []
     for name, step in steps:
         if step is None:
             fit_est = None
         else:
-            fit_est, Xt = do_fit_transform(dsk, step, Xt, y, param_lk[name])
+            fit_est, Xt = do_fit_transform(dsk, step, Xt, y, n_splits,
+                                           param_lk[name])
         fits.append(fit_est)
     return fits, Xt
 
 
-def _rebuild_pipeline(dsk, est, fits):
+def _rebuild_pipeline(dsk, est, fits, n_splits):
     names = [n for n, _ in est.steps]
     name = 'pipeline-' + tokenize(fits, names)
-    dsk[name] = (pipeline, names, fits)
+    for n in range(n_splits):
+        dsk[(name, n)] = (pipeline, names,
+                          [f if f is None else (f, n) for f in fits])
     return name
 
 
-def do_fit_transform_pipeline(dsk, est, X, y, fit_params):
+def do_fit_transform_pipeline(dsk, est, X, y, n_splits, fit_params):
     param_lk = _group_fit_params(est.steps, fit_params)
-    fits, Xt = _fit_transform_steps(dsk, est.steps, X, y, param_lk)
-    return _rebuild_pipeline(dsk, est, fits), Xt
+    fits, Xt = _fit_transform_steps(dsk, est.steps, X, y, n_splits, param_lk)
+    return _rebuild_pipeline(dsk, est, fits, n_splits), Xt
 
 
-def do_fit_pipeline(dsk, est, X, y, fit_params):
+def do_fit_pipeline(dsk, est, X, y, n_splits, fit_params):
     param_lk = _group_fit_params(est.steps, fit_params)
-    fits, Xt = _fit_transform_steps(dsk, est.steps[:-1], X, y, param_lk)
+    fits, Xt = _fit_transform_steps(dsk, est.steps[:-1], X, y, n_splits,
+                                    param_lk)
     name, step = est.steps[-1]
     fits.append(None if step is None
-                else do_fit(dsk, step, Xt, y, param_lk[name]))
-    return _rebuild_pipeline(dsk, est, fits)
+                else do_fit(dsk, step, Xt, y, n_splits, param_lk[name]))
+    return _rebuild_pipeline(dsk, est, fits, n_splits)
 
 
 # ------------ #
 # FeatureUnion #
 # ------------ #
 
-def _rebuild_feature_union(dsk, est, fits):
+def _rebuild_feature_union(dsk, est, fits, n_splits):
     names = [n for n, _ in est.transformer_list]
-    fit = tokenize(fits, names, est.transformer_weights)
-    dsk[fit] = (feature_union, fits, names, est.transformer_weights)
-    return fit
+    name = tokenize(fits, names, est.transformer_weights)
+    for n in range(n_splits):
+        dsk[(name, n)] = (feature_union, names,
+                          [f if f is None else (f, n) for f in fits],
+                          est.transformer_weights)
+    return name
 
 
-def do_fit_transform_feature_union(dsk, est, X, y, fit_params):
+def do_fit_transform_feature_union(dsk, est, X, y, n_splits, fit_params):
     param_lk = _group_fit_params(est.transformer_list, fit_params)
     get_weight = (est.transformer_weights or {}).get
     fits = []
@@ -171,23 +180,27 @@ def do_fit_transform_feature_union(dsk, est, X, y, fit_params):
         if tr is None:
             fit_est = None
         else:
-            fit_est, Xt = do_fit_transform(dsk, tr, X, y, param_lk[name])
+            fit_est, Xt = do_fit_transform(dsk, tr, X, y, n_splits,
+                                           param_lk[name])
             Xs.append(Xt)
             weights.append(get_weight(name))
         fits.append(fit_est)
 
     if not Xs:
         Xt = 'feature-union-transform-' + tokenize(X)
-        dsk[Xt] = (feature_union_empty, X)
+        for n in range(n_splits):
+            dsk[(Xt, n)] = (feature_union_empty, (X, n))
     else:
         Xt = 'feature-union-transform-' + tokenize(Xs, weights)
-        dsk[Xt] = (feature_union_concat, Xs, weights)
+        for n in range(n_splits):
+            dsk[(Xt, n)] = (feature_union_concat, [(x, n) for x in Xs], weights)
 
-    return _rebuild_feature_union(dsk, est, fits), Xt
+    return _rebuild_feature_union(dsk, est, fits, n_splits), Xt
 
 
-def do_fit_feature_union(dsk, est, X, y, fit_params):
+def do_fit_feature_union(dsk, est, X, y, n_splits, fit_params):
     param_lk = _group_fit_params(est.transformer_list, fit_params)
-    fits = [None if tr is None else do_fit(dsk, tr, X, y, param_lk[name])
+    fits = [None if tr is None
+            else do_fit(dsk, tr, X, y, n_splits, param_lk[name])
             for name, tr in est.transformer_list]
-    return _rebuild_feature_union(dsk, est, fits)
+    return _rebuild_feature_union(dsk, est, fits, n_splits)
