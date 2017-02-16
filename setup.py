e2dd1eb63536cd34a9b034e29353c73efe535b7e
Jim Crist <crist042@umn.edu>
2017-02-16 16:50:49 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,13 +1,13 @@
 from setuptools import setup
 
-install_requires = ["dask >= 0.8.2",
-                    "scikit-learn >= 0.17.1",
+install_requires = ["dask >= 0.12.0",
+                    "scikit-learn >= 0.18.1",
                     "numpy"]
 
 setup(name='dask-learn',
       version='0.1.0',
       license='BSD',
       description='Machine Learning with Dask',
-      url='http://github.com/jcrist/dask-learn',
+      url='http://github.com/dask/dask-learn',
       install_requires=install_requires,
-      packages=['dklearn'])
+      packages=['dklearn', 'dklearn.tests'])
