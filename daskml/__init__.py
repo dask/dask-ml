e32089e64e14ad1f7257661ebb22f809f663a5a4
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-06-13 16:22:12 -0500

    gh-pages

--- daskml/__init__.py
+++
@@ -1,3 +0,0 @@
-"""http://www.cs.cornell.edu/~kilian/papers/fr819-tyreeA.pdf"""
-
-__version__ = '0.1'
--- tests/test_gbrt.py
+++
@@ -1,9 +0,0 @@
-from gbrt.utils import make_data
-from gbrt.ensemble import GradientBoostingRegressor
-
-X, y = make_data()
-
-
-def test_smoke():
-    est = GradientBoostingRegressor()
-    est.fit(X, y)
