406d1e236442a7766a789141de7f42b1f36f4bb6
severo <danielsouzasevero@gmail.com>
2017-09-30 17:56:55 -0300

    flake8 now passing.

--- setup.py
+++ setup.py
@@ -10,7 +10,8 @@ here = os.path.abspath(os.path.dirname(__file__))
 with open(os.path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
-install_requires = ['dask', 'distributed', 'numpy', 'pandas', 'scikit-learn', 'scipy']
+install_requires = ['dask', 'distributed', 'numpy', 'pandas', 'scikit-learn',
+                    'scipy']
 
 # Optional Requirements
 
