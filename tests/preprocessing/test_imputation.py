342b66766355ce8e793040bc119cb610922dd421
severo <danielsouzasevero@gmail.com>
2017-10-15 19:01:28 -0200

    Refactored tests.

--- tests/preprocessing/test_imputation.py
+++ tests/preprocessing/test_imputation.py
@@ -16,42 +16,43 @@ df_with_zeros = X_with_zeros.to_dask_dataframe().rename(columns=str)
 df_with_nan = df_with_zeros.mask(df_with_zeros > 1)
 
 
+@pytest.mark.parametrize('strategy', ["mean"])
+@pytest.mark.parametrize('X,missing_values,columns', [
+    (X_with_nan, "NaN", None),
+    (X_with_nan, np.nan, None),
+    (X_with_zeros, 0, None),
+    (df_with_nan, "NaN", ['1', '2']),
+    (df_with_nan, np.nan, ['1', '2']),
+    (df_with_zeros, 0, ['1', '2']),
+    (df_with_zeros, 0, None)
+])
 class TestImputer(object):
-    @pytest.mark.parametrize('X,missing_values,columns', [
-        (X_with_nan, "NaN", None),
-        (X_with_nan, np.nan, None),
-        (X_with_zeros, 0, None),
-        (df_with_nan, "NaN", ['1', '2']),
-        (df_with_nan, np.nan, ['1', '2']),
-        (df_with_zeros, 0, ['1', '2']),
-        (df_with_zeros, 0, None)
-    ])
-    def test_fit(self, X, missing_values, columns):
-        a = dpp.Imputer(columns=columns, missing_values=missing_values)
-        b = spp.Imputer(missing_values=missing_values)
+    def test_fit(self, X, missing_values, columns, strategy):
+        a = dpp.Imputer(columns=columns, missing_values=missing_values,
+                        strategy=strategy)
+        b = spp.Imputer(missing_values=missing_values, strategy=strategy)
         columns_ix = list(map(int, columns) if columns else
                           range(len(X.columns) if isinstance(X, dd._Frame)
                                 else X.shape[1]))
+
+        if strategy == "median":
+            X = X.repartition(npartitions=1)
+
         a.fit(X)
         b.fit(X.compute())
         assert_eq_ar(a.statistics_.compute(),
                      b.statistics_[columns_ix])
 
-    @pytest.mark.parametrize('X,missing_values,columns', [
-        (X_with_nan, "NaN", None),
-        (X_with_nan, np.nan, None),
-        (X_with_zeros, 0, None),
-        (df_with_nan, "NaN", ['1', '2']),
-        (df_with_nan, np.nan, ['1', '2']),
-        (df_with_zeros, 0, ['1', '2']),
-        (df_with_zeros, 0, None)
-    ])
-    def test_fit_transform(self, X, missing_values, columns):
-        a = dpp.Imputer(columns=columns, missing_values=missing_values)
-        b = spp.Imputer(missing_values=missing_values)
+    def test_fit_transform(self, X, missing_values, columns, strategy):
+        a = dpp.Imputer(columns=columns, missing_values=missing_values,
+                        strategy=strategy)
+        b = spp.Imputer(missing_values=missing_values, strategy=strategy)
         columns_ix = list(map(int, columns) if columns else
                           range(len(X.columns) if isinstance(X, dd._Frame)
                                 else X.shape[1]))
+        if strategy == "median":
+            X = X.repartition(npartitions=1)
+
         ta = a.fit_transform(X).compute()
         tb = b.fit_transform(X.compute())[:, columns_ix]
         if isinstance(X, dd._Frame):
