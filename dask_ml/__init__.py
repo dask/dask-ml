3e0267821da9d3f451a919547fc1b84cfd13ab18
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2017-10-17 16:11:37 -0500

    Use setuptools_scm for automated versioning (#43)

--- dask_ml/__init__.py
+++ dask_ml/__init__.py
@@ -1,1 +1,6 @@
-__version__ = '0.1'
+from pkg_resources import get_distribution, DistributionNotFound
+try:
+    __version__ = get_distribution(__name__).version
+except DistributionNotFound:
+    # package is not installed
+    pass
