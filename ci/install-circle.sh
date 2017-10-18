ecd0d919ce6ef41d076377f1b544779e87863621
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-10-17 19:00:17 -0500

    CI: Run against scikit-learn dev

---
+++ ci/install-circle.sh
@@ -0,0 +1,9 @@
+apt-get update; apt-get install -y gcc
+conda config --set always_yes true --set changeps1 false --set quiet true
+conda update -q conda
+conda install conda-build anaconda-client --yes
+conda config --add channels conda-forge
+conda env create -f ci/environment.yml --name=${ENV_NAME}
+source activate ${ENV_NAME}
+pip install --no-deps -e .
+conda list ${ENV_NAME}
