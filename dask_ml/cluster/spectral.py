d44312cb2b111fa495e865eebf88dd26a6ff9de5
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-11-28 11:58:48 -0500

    Doc on callable affinity

--- dask_ml/cluster/spectral.py
+++ dask_ml/cluster/spectral.py
@@ -58,6 +58,11 @@ class SpectralClustering(BaseEstimator, ClusterMixin):
         increase with similarity) should be used. This property is not checked
         by the clustering algorithm.
 
+        Callables should expect arguments similar to
+       `sklearn.metrics.pairwise_kernels`: a required ``X``, an optional ``Y``,
+        and ``gamma``, ``degree``, ``coef0``, and any keywords passed in
+        ``kernel_params``.
+
     n_neighbors : integer
         Number of neighbors to use when constructing the affinity matrix using
         the nearest neighbors method. Ignored for ``affinity='rbf'``.
