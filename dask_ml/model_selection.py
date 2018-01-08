6f2fecfcbe94f6c4488739c9265f152ea6f9752b
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2018-01-08 15:25:39 -0600

    Updated notes on scalability of various estimators (#111)
    * updated notes on paralle
    
    * Fixed typo

--- dask_ml/model_selection.py
+++ dask_ml/model_selection.py
@@ -1,1 +1,6 @@
+"""Utilities for hyperparameter optimization.
+
+These estimators will operate in parallel. Their scalability depends
+on the underlying estimators being used.
+"""
 from dask_searchcv.model_selection import GridSearchCV, RandomizedSearchCV  # noqa
