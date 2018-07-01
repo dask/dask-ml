8acf604c61c2459ead0b23dfac2c3ae33f550922
Matthew Rocklin <mrocklin@gmail.com>
2018-07-01 19:46:16 -0400

    change pygments style to default

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -104,7 +104,7 @@ exclude_patterns = [
 exclude_trees = ['_build', 'includes']
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
