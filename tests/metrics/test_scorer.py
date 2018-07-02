087ddd0dcfeda3b6cd430e45af0a947494d81e04
Scott Sievert <github@stsievert.com>
2018-07-02 11:48:37 -0500

    MAINT: add failing test case

--- tests/metrics/test_scorer.py
+++ tests/metrics/test_scorer.py
@@ -4,6 +4,7 @@ from dask_ml.datasets import make_classification
 import dask.array as da
 from dask import delayed
 import pytest
+from sklearn.exceptions import NotFittedError
 
 
 def test_score_compute_basic():
@@ -25,3 +26,21 @@ def test_score_delayed_fit_and_score():
     inc = inc.partial_fit(X, y, classes=da.unique(y))
     s = inc.score(X, y)
     assert isinstance(s.compute(), float)
+
+
+@pytest.mark.parametrize("return_est", [True, False])
+def test_score_raises(return_est):
+    X, y = make_classification(random_state=0, chunks=20)
+    est = SGDClassifier(tol=1e-3)
+    inc = Incremental(est, scoring='accuracy')
+
+    inc = delayed(inc)
+    r = inc.partial_fit(X, y, classes=da.unique(y))
+    if return_est:
+        inc = r
+
+    if not return_est:
+        with pytest.raises(NotFittedError):
+            inc.score(X, y).compute()
+    else:
+        inc.score(X, y).compute()
