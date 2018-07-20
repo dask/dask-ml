8f49b9dc1d67c01b005ebd8ead16d709916613da
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2018-07-20 13:47:21 -0500

    ENH: LabelEncoder supports pandas Categorical (#310)

--- tests/preprocessing/test_label.py
+++ tests/preprocessing/test_label.py
@@ -1,3 +1,4 @@
+import dask
 import dask.array as da
 import dask.dataframe as dd
 import numpy as np
@@ -10,8 +11,8 @@ import sklearn.preprocessing as spp
 from dask_ml.utils import assert_estimator_equal
 
 choices = np.array(["a", "b", "c"], dtype=str)
-y = np.random.choice(choices, 100)
-y = da.from_array(y, chunks=13)
+np_y = np.random.choice(choices, 100)
+y = da.from_array(np_y, chunks=13)
 s = dd.from_array(y)
 
 
@@ -33,19 +34,27 @@ class TestLabelEncoder(object):
 
         a.fit(y)
         b.fit(y.compute())
-        assert_estimator_equal(a, b)
+        exclude = {"dtype_"}
+        assert_estimator_equal(a, b, exclude=exclude)
 
     def test_input_types(self, dask_array, pandas_series):
         a = dpp.LabelEncoder()
         b = spp.LabelEncoder()
 
-        assert_estimator_equal(a.fit(dask_array), b.fit(pandas_series))
+        exclude = {"dtype_"}
+        assert_estimator_equal(a.fit(dask_array), b.fit(pandas_series), exclude=exclude)
 
-        assert_estimator_equal(a.fit(pandas_series), b.fit(pandas_series))
+        assert_estimator_equal(
+            a.fit(pandas_series), b.fit(pandas_series), exclude=exclude
+        )
 
-        assert_estimator_equal(a.fit(pandas_series.values), b.fit(pandas_series))
+        assert_estimator_equal(
+            a.fit(pandas_series.values), b.fit(pandas_series), exclude=exclude
+        )
 
-        assert_estimator_equal(a.fit(dask_array), b.fit(pandas_series.values))
+        assert_estimator_equal(
+            a.fit(dask_array), b.fit(pandas_series.values), exclude=exclude
+        )
 
     @pytest.mark.parametrize("array", [y, s])
     def test_transform(self, array):
@@ -57,10 +66,17 @@ class TestLabelEncoder(object):
 
         assert_eq_ar(a.transform(array).compute(), b.transform(array.compute()))
 
-    @pytest.mark.parametrize("array", [y, s])
+    @pytest.mark.parametrize("array", [np_y, y, s])
     def test_transform_dtypes(self, array):
         result = dpp.LabelEncoder().fit_transform(array)
         assert result.dtype == np.intp
+        if dask.is_dask_collection(array):
+            assert result.dtype == result.compute().dtype
+
+    def test_fit_transform_categorical(self):
+        cat = dd.from_pandas(pd.Series(choices, dtype="category"), npartitions=4)
+        result = dpp.LabelEncoder().fit_transform(cat)
+        assert result.dtype == "int8"
         assert result.dtype == result.compute().dtype
 
     @pytest.mark.parametrize("array", [y, s])
@@ -68,3 +84,65 @@ class TestLabelEncoder(object):
 
         a = dpp.LabelEncoder()
         assert_eq_ar(a.inverse_transform(a.fit_transform(array)), da.asarray(array))
+
+    @pytest.mark.parametrize(
+        "categories, transformed",
+        [
+            (["a", "b", "c"], np.array([0, 1, 0], dtype=np.int8)),
+            (["a", "b"], np.array([0, 1, 0], dtype=np.int8)),
+            (["b", "a"], np.array([1, 0, 1], dtype=np.int8)),
+        ],
+    )
+    @pytest.mark.parametrize("daskify", [True, False, "unknown"])
+    @pytest.mark.parametrize("ordered", [True, False])
+    def test_categorical(self, categories, transformed, daskify, ordered):
+        cat = pd.Series(
+            ["a", "b", "a"],
+            dtype=pd.api.types.CategoricalDtype(categories=categories, ordered=ordered),
+        )
+        if daskify:
+            cat = dd.from_pandas(cat, npartitions=2)
+            transformed = da.from_array(transformed, chunks=(2, 1))
+            if daskify == "unknown":
+                cat = cat.cat.as_unknown()
+
+        a = dpp.LabelEncoder().fit(cat)
+
+        if daskify != "unknown":
+            assert a.dtype_ == cat.dtype
+        np.testing.assert_array_equal(a.classes_, categories)
+        result = a.transform(cat)
+        da.utils.assert_eq(result, transformed)
+
+        inv_transformed = a.inverse_transform(result)
+        if daskify:
+            # manually set the divisions for the test
+            inv_transformed.divisions = (0, 2)
+        dd.utils.assert_eq(inv_transformed, cat)
+
+    def test_dataframe_raises(self):
+        df = pd.DataFrame({"A": ["a", "a", "b"]}, dtype="category")
+        dpp.LabelEncoder().fit(df)  # OK
+
+        df["other"] = ["a", "b", "c"]
+        with pytest.raises(ValueError):
+            dpp.LabelEncoder().fit(df)
+
+    @pytest.mark.parametrize("daskify", [True, False])
+    def test_use_categorical(self, daskify):
+        data = pd.Series(
+            ["b", "c"], dtype=pd.api.types.CategoricalDtype(["c", "a", "b"])
+        )
+        if daskify:
+            data = dd.from_pandas(data, npartitions=2)
+        a = dpp.LabelEncoder(use_categorical=False).fit(data)
+        b = spp.LabelEncoder().fit(data)
+        assert_estimator_equal(a, b, exclude={"dtype_"})
+        assert a.dtype_ is None
+
+        da.utils.assert_eq(a.transform(data), b.transform(data))
+        a_trn = a.transform(data)
+        b_trn = b.transform(data)
+        da.utils.assert_eq(a_trn, b_trn)
+
+        da.utils.assert_eq(a.inverse_transform(a_trn), b.inverse_transform(b_trn))
