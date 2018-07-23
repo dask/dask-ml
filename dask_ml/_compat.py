d64a6f979880f45581b3eb96c54d23fed668c72a
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-07-23 10:28:01 -0500

    remove compat code

--- dask_ml/_compat.py
+++ dask_ml/_compat.py
@@ -1,11 +1,7 @@
 import contextlib
 
 import dask
-import dask.array as da
-import dask.dataframe as dd
-import numpy as np
 import packaging.version
-from scipy import sparse
 
 import sklearn
 
@@ -17,41 +13,3 @@ DASK_VERSION = packaging.version.parse(dask.__version__)
 @contextlib.contextmanager
 def dummy_context(*args, **kwargs):
     yield
-
-
-def hstack(tup):
-    """Horizontally stack some arrays.
-
-    Parameters
-    ----------
-    args : Iterable[array-like]
-        Supports scipy sparse arrays, dask dataframes, dask arrays
-        for NumPy arrays.
-    """
-    x = tup[0]
-
-    if sparse.issparse(x):
-        return sparse.hstack(tup)
-
-    elif isinstance(x, (dd.Series, dd.DataFrame)):
-        return dd.concat(tup, axis="columns")
-
-    elif isinstance(x, da.Array):
-        return da.hstack(tup)
-
-    else:
-        return np.hstack(tup)
-
-
-def ones_like(a, dtype=None, chunks=None):
-    if isinstance(a, da.Array):
-        return da.ones_like(a, dtype=dtype, chunks=chunks)
-    else:
-        return np.ones_like(a, dtype=dtype)
-
-
-def zeros_like(a, dtype=None, chunks=None):
-    if isinstance(a, da.Array):
-        return da.zeros_like(a, dtype=dtype, chunks=chunks)
-    else:
-        return np.zeros_like(a, dtype=dtype)
