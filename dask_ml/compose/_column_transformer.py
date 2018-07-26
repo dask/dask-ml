84a8e8cabd7dce65218155cf1dcb256bf52e5541
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-07-26 11:32:05 -0500

    Simplified implementation

--- dask_ml/compose/_column_transformer.py
+++ dask_ml/compose/_column_transformer.py
@@ -1,79 +1,28 @@
 import dask.array as da
 import dask.dataframe as dd
 import numpy as np
-import six
 import sklearn.compose
 from scipy import sparse
-from sklearn.compose._column_transformer import (
-    _check_key_type,
-    _fit_transform_one,
-    _get_transformer_list,
-    _transform_one,
-)
-from sklearn.utils.validation import check_is_fitted
+from sklearn.compose._column_transformer import _get_transformer_list
 
 
 class ColumnTransformer(sklearn.compose.ColumnTransformer):
-    def _validate_remainder(self, X):
+    @staticmethod
+    def _hstack(X):
         """
-        Validates ``remainder`` and defines ``_remainder`` targeting
-        the remaining columns.
-        """
-        # TODO(https://github.com/dask/dask/pull/3212)
-        # remove once dd.DataFrame.shape is implemented
-        # The only difference is the isinstance(X, dd.DataFrame) below.
-        is_transformer = (
-            hasattr(self.remainder, "fit") or hasattr(self.remainder, "fit_transform")
-        ) and hasattr(self.remainder, "transform")
-        if self.remainder not in ("drop", "passthrough") and not is_transformer:
-            raise ValueError(
-                "The remainder keyword needs to be one of 'drop', "
-                "'passthrough', or estimator. '%s' was passed instead" % self.remainder
-            )
+        Stacks X horizontally.
 
-        if isinstance(X, dd.DataFrame):
-            n_columns = len(X.columns)
+        Supports input types (X): list of
+            numpy arrays, sparse arrays and DataFrames
+        """
+        if any(sparse.issparse(f) for f in X):
+            return sparse.hstack(X).tocsr()
+        elif any(isinstance(f, (dd.Series, dd.DataFrame)) for f in X):
+            return dd.concat(X, axis="columns")
+        elif any(isinstance(f, da.Array) for f in X):
+            return da.hstack(X)
         else:
-            n_columns = X.shape[1]
-        cols = []
-        for _, _, columns in self.transformers:
-            cols.extend(_get_column_indices(X, columns))
-        remaining_idx = sorted(list(set(range(n_columns)) - set(cols))) or None
-
-        self._remainder = ("remainder", self.remainder, remaining_idx)
-
-    def fit_transform(self, X, y=None):
-        # TODO: remove this once we have a compatible _hstack.
-        # This is only impelmented here to use our _hstack.
-        self._validate_remainder(X)
-        self._validate_transformers()
-
-        result = self._fit_transform(X, y, _fit_transform_one)
-
-        if not result:
-            # All transformers are None
-            return np.zeros((X.shape[0], 0))
-
-        Xs, transformers = zip(*result)
-
-        self._update_fitted_transformers(transformers)
-        self._validate_output(Xs)
-
-        return _hstack(list(Xs))
-
-    def transform(self, X):
-        # TODO: remove this once we have a compatible _hstack.
-        # This is only impelmented here to use our _hstack.
-        check_is_fitted(self, "transformers_")
-
-        Xs = self._fit_transform(X, None, _transform_one, fitted=True)
-        self._validate_output(Xs)
-
-        if not Xs:
-            # All transformers are None
-            return np.zeros((X.shape[0], 0))
-
-        return _hstack(list(Xs))
+            return np.hstack(X)
 
 
 def make_column_transformer(*transformers, **kwargs):
@@ -87,82 +36,3 @@ def make_column_transformer(*transformers, **kwargs):
         )
     transformer_list = _get_transformer_list(transformers)
     return ColumnTransformer(transformer_list, n_jobs=n_jobs, remainder=remainder)
-
-
-def _get_column_indices(X, key):
-    """
-    Get feature column indices for input data X and key.
-
-    For accepted values of `key`, see the docstring of _get_column
-
-    """
-    # TODO(https://github.com/dask/dask/pull/3212)
-    # remove once dd.DataFrame.shape is implemented
-    # This if / else is the only difference
-    if isinstance(X, dd.DataFrame):
-        n_columns = len(X.columns)
-    else:
-        n_columns = X.shape[1]
-
-    if callable(key):
-        key = key(X)
-
-    if _check_key_type(key, int):
-        if isinstance(key, int):
-            return [key]
-        elif isinstance(key, slice):
-            return list(range(n_columns)[key])
-        else:
-            return list(key)
-
-    elif _check_key_type(key, six.string_types):
-        try:
-            all_columns = list(X.columns)
-        except AttributeError:
-            raise ValueError(
-                "Specifying the columns using strings is only "
-                "supported for pandas DataFrames"
-            )
-        if isinstance(key, six.string_types):
-            columns = [key]
-        elif isinstance(key, slice):
-            start, stop = key.start, key.stop
-            if start is not None:
-                start = all_columns.index(start)
-            if stop is not None:
-                # pandas indexing with strings is endpoint included
-                stop = all_columns.index(stop) + 1
-            else:
-                stop = n_columns + 1
-            return list(range(n_columns)[slice(start, stop)])
-        else:
-            columns = list(key)
-
-        return [all_columns.index(col) for col in columns]
-
-    elif hasattr(key, "dtype") and np.issubdtype(key.dtype, np.bool_):
-        # boolean mask
-        return list(np.arange(n_columns)[key])
-    else:
-        raise ValueError(
-            "No valid specification of the columns. Only a "
-            "scalar, list or slice of all integers or all "
-            "strings, or boolean mask is allowed"
-        )
-
-
-def _hstack(X):
-    """
-    Stacks X horizontally.
-
-    Supports input types (X): list of
-        numpy arrays, sparse arrays and DataFrames
-    """
-    if any(sparse.issparse(f) for f in X):
-        return sparse.hstack(X).tocsr()
-    elif any(isinstance(f, (dd.Series, dd.DataFrame)) for f in X):
-        return dd.concat(X, axis="columns")
-    elif any(isinstance(f, da.Array) for f in X):
-        return da.hstack(X)
-    else:
-        return np.hstack(X)
