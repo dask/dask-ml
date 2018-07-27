43dff42058b9bbece449adbb7f8e3e9831485689
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-07-27 13:38:51 -0500

    added control for pandas dataframes

--- dask_ml/compose/_column_transformer.py
+++ dask_ml/compose/_column_transformer.py
@@ -1,27 +1,50 @@
 import dask.array as da
 import dask.dataframe as dd
 import numpy as np
+import pandas as pd
 import sklearn.compose
 from scipy import sparse
 from sklearn.compose._column_transformer import _get_transformer_list
 
 
 class ColumnTransformer(sklearn.compose.ColumnTransformer):
-    def _hstack(self, X):
+    def __init__(
+        self,
+        transformers,
+        remainder="drop",
+        sparse_threshold=0.3,
+        n_jobs=1,
+        transformer_weights=None,
+        preserve_dataframe=True,
+    ):
+        self.preserve_dataframe = preserve_dataframe
+        super(ColumnTransformer, self).__init__(
+            transformers=transformers,
+            remainder=remainder,
+            sparse_threshold=sparse_threshold,
+            n_jobs=n_jobs,
+            transformer_weights=transformer_weights,
+        )
+
+    def _hstack(self, Xs):
         """
         Stacks X horizontally.
 
         Supports input types (X): list of
             numpy arrays, sparse arrays and DataFrames
         """
+        types = set(type(X) for X in Xs)
+
         if self.sparse_output_:
-            return sparse.hstack(X).tocsr()
-        elif any(isinstance(f, (dd.Series, dd.DataFrame)) for f in X):
-            return dd.concat(X, axis="columns")
-        elif any(isinstance(f, da.Array) for f in X):
-            return da.hstack(X)
+            return sparse.hstack(Xs).tocsr()
+        elif dd.Series in types or dd.DataFrame in types:
+            return dd.concat(Xs, axis="columns")
+        elif da.Array in types:
+            return da.hstack(Xs)
+        elif self.preserve_dataframe and (pd.Series in types or pd.DataFrame in types):
+            return pd.concat(Xs, axis="columns")
         else:
-            return np.hstack(X)
+            return np.hstack(Xs)
 
 
 def make_column_transformer(*transformers, **kwargs):
