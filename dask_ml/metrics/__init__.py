3834756e08a7dc63c3419e9445852e1dd6a48f14
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2018-07-27 09:34:15 -0500

    Log loss metric (#318)
    * log_loss sample weight
    
    * log_loss sample weight
    
    * cleanup
    
    * sample_weight
    
    * sort

--- dask_ml/metrics/__init__.py
+++ dask_ml/metrics/__init__.py
@@ -4,6 +4,6 @@ from .pairwise import (  # noqa
     euclidean_distances,
 )
 from .regression import mean_absolute_error, mean_squared_error, r2_score  # noqa
-from .classification import accuracy_score  # noqa
+from .classification import accuracy_score, log_loss  # noqa
 
 from .scorer import get_scorer, check_scoring, SCORERS  # noqa
