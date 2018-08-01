5f4151255c4c21a24441ede82a3acdfa05563f1e
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-01 16:25:12 -0500

    Just testing

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,7 +65,7 @@ sphinx_gallery_conf = {
     "backreferences_dir": False,
 }
 
-nbsphinx_timeout = 120
+nbsphinx_timeout = 600
 numpydoc_class_members_toctree = False
 autodoc_default_flags = ["members", "inherited-members"]
 autosummary_generate = True
