956d65fc75352afd64ab54f17fbe93b94fc700b6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-28 07:39:38 -0500

    Reinstall apt-get

--- ci/install-circle.sh
+++ ci/install-circle.sh
@@ -1,5 +1,6 @@
 set -xe
 
+apt-get update; apt-get install -y gcc g++
 conda config --set always_yes true --set changeps1 false --set quiet true
 conda update -q conda
 conda install conda-build anaconda-client --yes --quiet
