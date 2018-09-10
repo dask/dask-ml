efc898d705c5e7f11e41ad95881dc1c6b335f05b
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-09-10 08:07:41 -0500

    theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,6 @@ import subprocess
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-import dask_sphinx_theme
 
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -118,9 +117,8 @@ todo_include_todos = False
 # -- Options for HTML output ----------------------------------------------
 
 
-html_theme = "dask_sphinx_theme"
-
-html_theme_path = [dask_sphinx_theme.get_html_theme_path()]
+# html_theme = "dask_sphinx_theme"
+# html_theme_path = [dask_sphinx_theme.get_html_theme_path()]
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
