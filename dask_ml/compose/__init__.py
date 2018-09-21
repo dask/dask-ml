1a0f2c9a5ed27baa6e28365b0448e75eb2d60b03
James Bourbeau <jrbourbeau@gmail.com>
2018-09-20 20:11:56 -0500

    Adds sklearn version check to compose/__init__.py

--- dask_ml/compose/__init__.py
+++ dask_ml/compose/__init__.py
@@ -2,6 +2,15 @@
 
 These estimators are useful for working with heterogenous tabular data.
 """
-from ._column_transformer import ColumnTransformer, make_column_transformer
+from .._compat import SK_VERSION
+from packaging.version import parse
 
-__all__ = ["ColumnTransformer", "make_column_transformer"]
+__all__ = []
+
+if SK_VERSION >= parse("0.20.0.dev0"):
+    from ._column_transformer import ColumnTransformer, make_column_transformer  # noqa
+
+    __all__.extend(["ColumnTransformer", "make_column_transformer"])
+
+del SK_VERSION
+del parse
