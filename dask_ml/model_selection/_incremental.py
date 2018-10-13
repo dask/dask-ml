c4cfcdf28116afa3d13d56c1c4c7b109b6074cce
Scott Sievert <github@stsievert.com>
2018-10-13 12:17:55 -0500

    API: max_iter=100 (resolves confict between docs and impl)

--- dask_ml/model_selection/_incremental.py
+++ dask_ml/model_selection/_incremental.py
@@ -848,7 +848,7 @@ class IncrementalSearchCV(BaseIncrementalSearchCV):
         patience=False,
         tol=0.001,
         scores_per_fit=1,
-        max_iter=None,
+        max_iter=100,
         random_state=None,
         scoring=None,
     ):
