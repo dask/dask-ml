14852e1f102c895f83f1abb040bba66aa48c6e2d
J42994 <jan-christopher.koch@eon.com>
2018-10-23 21:06:09 +0200

    first transform implementation

--- dask_ml/preprocessing/_block_transformer.py
+++ dask_ml/preprocessing/_block_transformer.py
@@ -13,4 +13,47 @@ from dask_ml.utils import check_array, handle_zeros_in_scale
 
 
 class BlockTransformer(FunctionTransformer):
-    pass
+    def __init__(
+        self,
+        func=None,
+        inverse_func=None,
+        validate=None,
+        accept_sparse=False,
+        check_inverse=True,
+        kw_args=None,
+        inv_kw_args=None,
+    ):
+        super(BlockTransformer, self).__init__(
+            func=func,
+            inverse_func=inverse_func,
+            validate=validate,
+            accept_sparse=accept_sparse,
+            check_inverse=check_inverse,
+            kw_args=kw_args,
+            inv_kw_args=inv_kw_args,
+        )
+
+    def fit(self, X):
+        self._transformer = FunctionTransformer(
+            func=self.func,
+            inverse_func=self.inverse_func,
+            accept_sparse=self.accept_sparse,
+            check_inverse=self.check_inverse,
+            kw_args=self.kw_args,
+            inv_kw_args=self.inv_kw_args,
+        )
+        return self
+
+    def transform(self, X):
+        if isinstance(X, da.Array):
+            X = check_array(X, accept_dask_array=True, accept_unknown_chunks=True)
+            XP = X.map_blocks(
+                self._transformer.transform, dtype=X.dtype, chunks=X.chunks
+            )
+        else:
+            X = check_array(X, accept_dask_array=False)
+            XP = self._transformer.transform(X)
+        return XP
+
+    def inverse_transform(self, X):
+        pass
--- tests/preprocessing/test_block_transformer.py
+++ tests/preprocessing/test_block_transformer.py
@@ -13,3 +13,18 @@ from dask.dataframe.utils import assert_eq as assert_eq_df
 import dask_ml.preprocessing as dpp
 from dask_ml.datasets import make_classification
 from dask_ml.utils import assert_estimator_equal
+
+X = np.arange(100).reshape((25, 4))
+X_dask = da.from_array(X, chunks=(5, 4))
+
+
+class TestBlockTransformer:
+    @pytest.mark.filterwarnings("ignore::FutureWarning")
+    def test_identity(self):
+        block_trans = dpp.BlockTransformer()
+        func_trans = spp.FunctionTransformer()
+
+        X_t = func_trans.fit_transform(X)
+        X_dask_t = block_trans.fit_transform(X)
+        # assert dask.is_dask_collection(X_dask_t)
+        assert_eq_ar(X_t, X_dask_t)
