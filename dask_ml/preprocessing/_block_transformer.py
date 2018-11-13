b58a59db3685b4e2c9b00ffc6093a21cbb8b2008
J42994 <jan-christopher.koch@eon.com>
2018-11-13 21:31:27 +0100

    removes unnecessary imports

--- dask_ml/preprocessing/_block_transformer.py
+++ dask_ml/preprocessing/_block_transformer.py
@@ -1,15 +1,6 @@
 import dask.array as da
-import dask.dataframe as dd
-import numpy as np
-import pandas as pd
-from dask import compute
-from pandas.api.types import is_categorical_dtype
-from scipy import stats
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.preprocessing import FunctionTransformer
-from sklearn.utils.validation import check_is_fitted, check_random_state
 
-from dask_ml._utils import copy_learned_attributes
 from dask_ml.utils import check_array, handle_zeros_in_scale
 
 
