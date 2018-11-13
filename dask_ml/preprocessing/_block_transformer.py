6ceb531236e816d7def6cdca56345fa9c283e29f
J42994 <jan-christopher.koch@eon.com>
2018-11-13 21:20:41 +0100

    implements elementary block transformer

--- dask_ml/preprocessing/_block_transformer.py
+++ dask_ml/preprocessing/_block_transformer.py
@@ -5,6 +5,7 @@ import pandas as pd
 from dask import compute
 from pandas.api.types import is_categorical_dtype
 from scipy import stats
+from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.preprocessing import FunctionTransformer
 from sklearn.utils.validation import check_is_fitted, check_random_state
 
@@ -12,40 +13,48 @@ from dask_ml._utils import copy_learned_attributes
 from dask_ml.utils import check_array, handle_zeros_in_scale
 
 
-class BlockTransformer(FunctionTransformer):
-    def __init__(
-        self,
-        func=None,
-        inverse_func=None,
-        validate=None,
-        accept_sparse=False,
-        check_inverse=True,
-        kw_args=None,
-        inv_kw_args=None,
-    ):
-        super(BlockTransformer, self).__init__(
-            func=func,
-            inverse_func=inverse_func,
-            validate=validate,
-            accept_sparse=accept_sparse,
-            check_inverse=check_inverse,
-            kw_args=kw_args,
-            inv_kw_args=inv_kw_args,
-        )
+class BlockTransformer(BaseEstimator, TransformerMixin):
+    """Construct a transformer from a an arbitrary callable
+
+    The BlockTransformer forwards the blocks of the X arguments to a user-defined
+    function or function-object an returns the result of this operation.
+    This is useful for stateless operations, that can be performed on the cell or
+    block level, such as taking the log of frequencies. In general the transformer
+    is not suitable for e.g. standardization tasks as this requires information for
+    a complete columns
+
+    Parameters
+    ----------
+    func : callable
+        The callable to use for the transformation.
+
+    validate : bool, optional default=True
+         Indicate that the input X array should be checked before calling
+        ``func``.
+
+    kw_args : dict, optional
+        Dictionary of additional keyword arguments to pass to func.
+    """
+
+    def __init__(self, func=None, validate=True, kw_args=None):
+        self.func = func
+        self.validate = validate
+        self.accept_sparse = False
+        self.kw_args = kw_args
 
     def fit(self, X):
         return self
 
     def transform(self, X):
-        if isinstance(X, da.Array):
-            X = check_array(X, accept_dask_array=True, accept_unknown_chunks=True)
-            XP = X.map_blocks(
-                super(BlockTransformer, self).transform, dtype=X.dtype, chunks=X.chunks
-            )
-        else:
-            X = check_array(X, accept_dask_array=False)
-            XP = super(BlockTransformer, self).transform(X)
-        return XP
+        def func_forwarded(arr):
+            return self.func(arr, **(self.kw_args if self.kw_args else {}))
 
-    def inverse_transform(self, X):
-        pass
+        if isinstance(X, da.Array):
+            if self.validate:
+                X = check_array(X, accept_dask_array=True, accept_unknown_chunks=True)
+            XP = X.map_blocks(func_forwarded, dtype=X.dtype, chunks=X.chunks)
+        else:
+            if self.validate:
+                X = check_array(X, accept_dask_array=False)
+            XP = func_forwarded(X)
+        return XP
