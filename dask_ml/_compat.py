465fca65106036b4ed39d3203bc9a07649d4c92b
James Bourbeau <jrbourbeau@gmail.com>
2019-02-15 22:30:13 -0600

    Fix pandas SparseDtype failures

--- dask_ml/_compat.py
+++ dask_ml/_compat.py
@@ -2,11 +2,13 @@ import contextlib
 
 import dask
 import packaging.version
+import pandas
 import six
 import sklearn
 
 SK_VERSION = packaging.version.parse(sklearn.__version__)
 DASK_VERSION = packaging.version.parse(dask.__version__)
+PANDAS_VERSION = packaging.version.parse(pandas.__version__)
 
 
 @contextlib.contextmanager
