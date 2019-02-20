159ee67b882cd232df3e24d2cf9a5eda54f2c1de
Scott Sievert <dev@stsievert.com>
2019-02-19 21:17:25 -0600

    Typo

--- tests/model_selection/test_no_distributed.py
+++ tests/model_selection/test_no_distributed.py
@@ -10,5 +10,5 @@ except ImportError:
 
 @pytest.mark.skipif(HAS_DISTRIBUTED, reason="has package `distribtued`")
 def test_incremental_no_distributed():
-    with pytest.warns(ImportWarning, match="foo"):
+    with pytest.warns(ImportWarning):
         import dask_ml.model_selection
