16f667ee435b839d07297c9316b2b59976c8d6f7
Scott Sievert <dev@stsievert.com>
2019-04-08 10:06:29 -0500

    isort (new version *sigh*)

--- dask_ml/model_selection/__init__.py
+++ dask_ml/model_selection/__init__.py
@@ -3,12 +3,11 @@
 These estimators will operate in parallel. Their scalability depends
 on the underlying estimators being used.
 """
-from ._search import GridSearchCV, RandomizedSearchCV, compute_n_splits, check_cv
-from ._split import ShuffleSplit, KFold, train_test_split
-from ._incremental import IncrementalSearchCV
 from ._hyperband import HyperbandSearchCV
+from ._incremental import IncrementalSearchCV
+from ._search import GridSearchCV, RandomizedSearchCV, check_cv, compute_n_splits
+from ._split import KFold, ShuffleSplit, train_test_split
 from ._successive_halving import SuccessiveHalvingSearchCV
-
 
 __all__ = [
     "GridSearchCV",
