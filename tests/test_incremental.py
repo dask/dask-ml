4b8a384fbc332a3652a7788774267379faffae4c
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-05-20 08:52:19 -0500

    fixups

--- tests/test_incremental.py
+++ tests/test_incremental.py
@@ -9,6 +9,7 @@ from sklearn.base import clone
 from sklearn.linear_model import SGDClassifier, SGDRegressor
 
 import dask_ml.metrics
+from dask_ml._compat import SK_022
 from dask_ml.metrics.scorer import check_scoring
 from dask_ml.wrappers import Incremental
 
@@ -91,7 +92,11 @@ def test_in_gridsearch(scheduler, xy_classification):
     X, y = xy_classification
     clf = Incremental(SGDClassifier(random_state=0, tol=1e-3))
     param_grid = {"estimator__alpha": [0.1, 10]}
-    gs = sklearn.model_selection.GridSearchCV(clf, param_grid, iid=False, cv=3)
+    if SK_022:
+        kwargs = {}
+    else:
+        kwargs = {"iid": False}
+    gs = sklearn.model_selection.GridSearchCV(clf, param_grid, cv=3, **kwargs)
 
     with scheduler() as (s, [a, b]):
         gs.fit(X, y, classes=[0, 1])
