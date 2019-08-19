5a12a2ace2b58066f2364e6362dd1380e47ff73e
James Bourbeau <jrbourbeau@users.noreply.github.com>
2019-08-19 07:23:14 -0400

    Add dask.dataframe as dependency (#538)

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 install_requires = [
-    "dask[array]>=1.0.0",
+    "dask[array,dataframe]>=1.0.0",
     "distributed>=1.25.0",
     "numba",
     "numpy",
