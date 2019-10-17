fb327a35c6a68a2d22cd938576c4cb84a8a69864
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-10-17 10:16:00 -0500

    bump for array_function

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ with open(os.path.join(here, "README.rst"), encoding="utf-8") as f:
     long_description = f.read()
 
 install_requires = [
-    "dask[array,dataframe]>=1.0.0",
+    "dask[array,dataframe]>=1.2.0",
     "distributed>=1.25.0",
     "numba",
-    "numpy",
+    "numpy>=1.17.0",
     "pandas>=0.23.4",
     "scikit-learn>=0.20",
     "scipy",
