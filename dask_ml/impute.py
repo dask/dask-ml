751e8e1079028dfedaf2058d5851f117166ae908
Bonesters <Bonesters@users.noreply.github.com>
2019-10-29 19:47:53 -0400

    fix for issue #567
    SimpleImputer will now try and convert columns to numeric dtypes when using mean or median strategy for dask dataframes, as to give a more easily understandable error if non-numeric columns are present in a dataframe

--- dask_ml/impute.py
+++ dask_ml/impute.py
@@ -67,9 +67,19 @@ class SimpleImputer(sklearn.impute.SimpleImputer):
 
     def _fit_frame(self, X):
         if self.strategy == "mean":
-            avg = X.mean(axis=0).values
+            if isinstance(X,pd.DataFrame):
+                avg = X.mean(axis=0,numeric_only=False).values
+            else:
+                for col in X.columns:
+                    X[col] = X[col].astype(np.number)
+                avg = X.mean(axis=0).values
         elif self.strategy == "median":
-            avg = X.quantile().values
+            if isinstance(X,pd.DataFrame):
+                avg = X.quantile(numeric_only=False).values
+            else:
+                for col in X.columns:
+                    X[col] = X[col].astype(np.number)
+                avg = X.quantile().values
         elif self.strategy == "constant":
             avg = np.full(len(X.columns), self.fill_value)
         else:
