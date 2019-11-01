b4d0c9e330f230821e3e4ae053d978c822dcd7f6
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-11-01 16:05:45 -0500

    ENH: Added FeatureHasher

--- tests/feature_extraction/test_text.py
+++ tests/feature_extraction/test_text.py
@@ -21,6 +21,34 @@ JUNK_FOOD_DOCS = (
 
 
 @pytest.mark.parametrize("container", ["bag", "series", "array"])
+@pytest.mark.parametrize(
+    "vect",
+    [
+        dask_ml.feature_extraction.text.HashingVectorizer(),
+        dask_ml.feature_extraction.text.FeatureHasher(input_type="string"),
+    ],
+)
+def test_basic(vect, container):
+    b = db.from_sequence(JUNK_FOOD_DOCS, npartitions=2)
+    if container == "series":
+        b = b.to_dataframe(columns=["text"])["text"]
+    elif container == "array":
+        b = b.to_dataframe(columns=["text"])["text"].values
+
+    vect_ref = vect._hasher(**vect.get_params())
+    X_ref = vect_ref.fit_transform(b.compute())
+    X_da = vect.fit_transform(b)
+
+    assert_estimator_equal(vect_ref, vect)
+
+    assert isinstance(X_da, da.Array)
+    assert isinstance(X_da.blocks[0].compute(), scipy.sparse.csr_matrix)
+    result = X_da.map_blocks(lambda x: x.toarray(), dtype=X_da.dtype)
+    expected = X_ref.toarray()
+    np.testing.assert_array_equal(result, expected)
+
+
+@pytest.mark.parametrize("container", ["bag", "series", "array"])
 def test_hashing_vectorizer(container):
     b = db.from_sequence(JUNK_FOOD_DOCS, npartitions=2)
     if container == "series":
