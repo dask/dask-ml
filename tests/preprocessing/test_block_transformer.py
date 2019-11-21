47c33006cc69d3923a9a199621c5135a178a2baa
datajanko <jan.koch@tu-dortmund.de>
2019-11-21 21:07:02 +0100

    adds tests for kwargs to pass function arguments

--- tests/preprocessing/test_block_transformer.py
+++ tests/preprocessing/test_block_transformer.py
@@ -13,6 +13,8 @@ X_dask = da.from_array(X, chunks=(5, 4))
 df_dask = X_dask.to_dask_dataframe().rename(columns=str)
 df = df_dask.compute()
 
+def multiply(x, factor=4):
+    return factor * x
 
 class TestBlockTransformer:
     @pytest.mark.parametrize("func", [lambda x: 2 * x])
@@ -31,6 +33,23 @@ class TestBlockTransformer:
             assert dask.is_dask_collection(bt.transform(df))
         da.utils.assert_eq(bt.transform(X), func(X))
         dd.utils.assert_eq(bt.transform(df), func(df))
+    
+    @pytest.mark.parametrize("factor", [2, 4])
+    @pytest.mark.parametrize("validation", [True, False])
+    @pytest.mark.parametrize("daskify", [True, False])
+    def test_block_transform_multiply(self, daskify, validation, factor):
+        X = np.arange(100).reshape((25, 4))
+        df = pd.DataFrame(X).rename(columns=str)
+        if daskify:
+            X = da.from_array(X, chunks=(5, 4))
+            df = dd.from_pandas(df, npartitions=2)
+        bt = BlockTransformer(multiply, validate=validation, factor=factor)
+
+        if daskify:
+            assert dask.is_dask_collection(bt.transform(X))
+            assert dask.is_dask_collection(bt.transform(df))
+        da.utils.assert_eq(bt.transform(X), multiply(X, factor=factor))
+        dd.utils.assert_eq(bt.transform(df), multiply(df, factor=factor))
 
     @pytest.mark.parametrize("validate", [True, False])
     @pytest.mark.parametrize("daskify", [True, False])
