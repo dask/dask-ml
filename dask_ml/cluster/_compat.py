420df68224d441d1b0d9f15c67be53d0e20a6c8c
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-06 08:07:13 -0600

    fixup

--- dask_ml/cluster/_compat.py
+++ dask_ml/cluster/_compat.py
@@ -1,7 +1,7 @@
 from .._compat import SK_022
 
 if SK_022:
-    from sklearn.cluster._k_means import _k_init
+    from sklearn.cluster._kmeans import _k_init
 else:
     from sklearn.cluster.k_means_ import _k_init
 
