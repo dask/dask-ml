0e5ceaea03c1f66cbf0a3940d44d75d1392f6d41
Yuuichi ASAHI <asahi.yuuichi@qst.go.jp>
2020-02-27 11:53:32 +0900

    compute inside partial_fit

--- dask_ml/decomposition/incremental_pca.py
+++ dask_ml/decomposition/incremental_pca.py
@@ -10,6 +10,7 @@ from dask import array as da
 from dask.array import linalg
 from dask import delayed
 
+from dask import compute
 from sklearn.utils import gen_batches
 from sklearn.utils.validation import check_random_state
 from ..utils import check_array
@@ -271,21 +272,77 @@ class IncrementalPCA(PCA):
             )
         U, V = svd_flip(U, V)
         explained_variance = S ** 2 / (n_total_samples - 1)
-        explained_variance_ratio = S ** 2 / np.sum(col_var * n_total_samples)
+        components, singular_values = V, S
+
+        if solver == "randomized":
+            total_var = X.var(ddof=1, axis=0).sum()
+        else:
+            total_var = explained_variance.sum()
+
+        explained_variance_ratio = explained_variance / total_var
 
         self.n_samples_seen_ = n_total_samples
-        self.components_ = V[:self.n_components_]
-        self.singular_values_ = S[:self.n_components_]
-        self.mean_ = col_mean
-        self.var_ = col_var
-        self.explained_variance_ = explained_variance[:self.n_components_]
-        self.explained_variance_ratio_ = \
-            explained_variance_ratio[:self.n_components_]
-        if self.n_components_ < n_features:
-            self.noise_variance_ = \
-                explained_variance[self.n_components_:].mean()
+
+        if self.n_components_ < min(n_features, n_samples):
+            if solver == "randomized":
+                noise_variance = (total_var.sum() - explained_variance.sum()) / (
+                    min(n_features, n_samples) - n_components
+                )
+            else:
+                noise_variance = \
+                    da.mean(explained_variance[self.n_components_:])
         else:
-            self.noise_variance_ = 0.
+            noise_variance = 0.
+
+        try:
+            (
+                self.n_samples_,
+                self.mean_,
+                self.var_,
+                self.n_features_,
+                self.components_,
+                self.explained_variance_,
+                self.explained_variance_ratio_,
+                self.singular_values_,
+                self.noise_variance_,
+            ) = compute(
+                n_samples,
+                col_mean,
+                col_var,
+                n_features,
+                components,
+                explained_variance,
+                explained_variance_ratio,
+                singular_values,
+                noise_variance,
+            )
+        except ValueError as e:
+            if np.isnan([n_samples, n_features]).any():
+                msg = (
+                    "Computation of the SVD raised an error. It is possible "
+                    "n_components is too large. i.e., "
+                    "`n_components > np.nanmin(X.shape) = "
+                    "np.nanmin({})`\n\n"
+                    "A possible resolution to this error is to ensure that "
+                    "n_components <= min(n_samples, n_features)"
+                )
+                raise ValueError(msg.format(X.shape)) from e
+            raise e
+
+        self.components_ = self.components_[:self.n_components_]
+        self.singular_values_ = self.singular_values_[:self.n_components_]
+        self.explained_variance_ = self.explained_variance_[:self.n_components_]
+        self.explained_variance_ratio_ = \
+            self.explained_variance_ratio_[:self.n_components_]
+
+        if len(self.singular_values_) < self.n_components_:
+            self.n_components_ = len(self.singular_values_)
+            msg = (
+                "n_components={n} is larger than the number of singular values"
+                " ({s}) (note: PCA has attributes as if n_components == {s})"
+            )
+            raise ValueError(msg.format(n=self.n_components_, s=len(self.singular_values_)))
+
         return self
 
     def get_covariance(self):
