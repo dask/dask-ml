6607e91f383a0e75bc58bbbcb50df65fe04ffa97
Prasun Anand <prasunanand.bitsp@gmail.com>
2020-03-27 15:13:57 +0530

    Add _typing for ArrayLike

---
+++ dask_ml/_typing.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+from typing import TypeVar
+
+import numpy as np
+
+from dask.array import Array
+
+if TYPE_CHECKING:
+    from pandas.core.arrays.base import ExtensionArray  # noqa: F401
+    from pandas.core.indexes.base import Index  # noqa: F401
+    from pandas.core.series import Series  # noqa: F401
+
+# array-like
+
+AnyArrayLike = TypeVar(
+    "AnyArrayLike", "ExtensionArray", "Index", "Series", "Array", np.ndarray
+)
+ArrayLike = TypeVar("ArrayLike", "ExtensionArray", "Array", np.ndarray)
