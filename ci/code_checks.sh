bd9b9ef88852fe0dada566e42452b9f5668e1e6f
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-04-01 16:49:55 -0500

    skip codecov

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -13,8 +13,4 @@ MSG='Checking isort... ' ; echo $MSG
 isort --recursive --check-only .
 RET=$(($RET + $?)) ; echo $MSG "DONE"
 
-MSG='Checking coverage... ' ; echo $MSG
-bash <(curl -s https://codecov.io/bash)
-RET=$(($RET + $?)) ; echo $MSG "DONE"
-
 exit $RET
