975b6a1d69d36ec3cb89d06af8de1a122687a637
Scott <stsievert@users.noreply.github.com>
2020-04-14 17:33:42 -0500

    note

--- tests/model_selection/test_incremental.py
+++ tests/model_selection/test_incremental.py
@@ -670,7 +670,7 @@ def test_verbosity(Search, verbose, capsys):
     # IncrementalSearchCV always logs to INFO
     with captured_logger(logging.getLogger("dask_ml.model_selection")) as logs:
         search = _test_verbosity()
-        assert search.best_score_ > 0
+        assert search.best_score_ > 0  # make extra-sure the search ran
         messages = logs.getvalue().splitlines()
 
     # Make sure we always log
