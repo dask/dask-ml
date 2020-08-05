9fedf60a2bad676713762c66a5ede4270d0a1d1f
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-08-05 11:12:36 -0500

    optimize fit, transform

--- dask_ml/feature_extraction/text.py
+++ dask_ml/feature_extraction/text.py
@@ -206,20 +206,20 @@ class CountVectorizer(sklearn.feature_extraction.text.CountVectorizer):
 
         if vocabulary is None:
             check_is_fitted(self, "vocabulary_")
-            vocabulary_for_transform = self.vocabulary_
-        else:
-            if isinstance(vocabulary, dict):
-                # scatter for the user
-                try:
-                    client = get_client()
-                except ValueError:
-                    vocabulary_for_transform = dask.delayed(vocabulary)
-                else:
-                    (vocabulary_for_transform,) = client.scatter(
-                        (vocabulary,), broadcast=True
-                    )
+            vocabulary = self.vocabulary_
+
+        if isinstance(vocabulary, dict):
+            # scatter for the user
+            try:
+                client = get_client()
+            except ValueError:
+                vocabulary_for_transform = dask.delayed(vocabulary)
             else:
-                vocabulary_for_transform = vocabulary
+                (vocabulary_for_transform,) = client.scatter(
+                    (vocabulary,), broadcast=True
+                )
+        else:
+            vocabulary_for_transform = vocabulary
 
         n_features = vocabulary_length(vocabulary_for_transform)
         transformed = raw_documents.map_partitions(
