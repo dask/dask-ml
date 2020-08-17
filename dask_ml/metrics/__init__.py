c64b2176148816b117047af906728fb3a348462c
Ray Bell <rayjohnbell0@gmail.com>
2020-08-17 09:34:04 -0400

    ENH: add mean_squared_log_error (#725)
    * ENH: add mean_squared_log_error

--- dask_ml/metrics/__init__.py
+++ dask_ml/metrics/__init__.py
@@ -4,5 +4,10 @@ from .pairwise import (  # noqa
     pairwise_distances,
     pairwise_distances_argmin_min,
 )
-from .regression import mean_absolute_error, mean_squared_error, r2_score  # noqa
+from .regression import (  # noqa
+    mean_absolute_error,
+    mean_squared_error,
+    mean_squared_log_error,
+    r2_score,
+)
 from .scorer import SCORERS, check_scoring, get_scorer  # noqa
