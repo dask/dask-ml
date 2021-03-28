8a6f5a6265bdaeae40e9f15f3ddaa96263ed270b
Hristo Georgiev <hristo.a.georgiev@gmail.com>
2021-03-28 15:15:26 +0000

    Enable black and isort to play nicely together

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -12,7 +12,7 @@ RET=$(($RET + $?)) ; echo $MSG "DONE"
 
 MSG='Checking isort... ' ; echo $MSG
 isort --version-number
-isort --recursive --check-only .
+isort --profile=black --check-only .
 RET=$(($RET + $?)) ; echo $MSG "DONE"
 
 MSG='Checking mypy... ' ; echo $MSG
