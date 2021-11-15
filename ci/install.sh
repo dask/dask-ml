67c0c434961a39fdef253770ce608e12bc5c7aad
James Bourbeau <jrbourbeau@gmail.com>
2021-11-15 14:25:52 -0600

    Add upstream CI build

--- ci/install.sh
+++ ci/install.sh
@@ -1,3 +1,25 @@
+
+# Optionally, install development versions of dependenies
+if [[ ${UPSTREAM_DEV} ]]; then
+    # FIXME https://github.com/mamba-org/mamba/issues/412
+    # mamba uninstall --force numpy pandas
+    conda uninstall --force numpy pandas
+
+    python -m pip install --no-deps --pre \
+        -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
+        numpy \
+        pandas \
+        scikit-learn
+
+    python -m pip install \
+        --upgrade \
+        locket \
+        git+https://github.com/pydata/sparse \
+        git+https://github.com/dask/dask \
+        git+https://github.com/dask/distributed
+fi
+
+# Install dask-ml
 python -m pip install --quiet --no-deps -e .
 
 echo mamba list
