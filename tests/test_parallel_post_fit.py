25bbfca96223cab3e60966a87fced57b12f1c101
Vibhu Jawa <vibhujawa@gmail.com>
2021-11-16 11:15:45 -0800

    Added meta tests for predict, predict_proba, transform

--- tests/test_parallel_post_fit.py
+++ tests/test_parallel_post_fit.py
@@ -2,11 +2,14 @@ import dask
 import dask.array as da
 import dask.dataframe as dd
 import numpy as np
+import pandas as pd
 import pytest
 import sklearn.datasets
 from sklearn.decomposition import PCA
 from sklearn.ensemble import GradientBoostingClassifier
 from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.naive_bayes import CategoricalNB
+from sklearn.preprocessing import OneHotEncoder
 
 from dask_ml.datasets import make_classification
 from dask_ml.utils import assert_eq_ar, assert_estimator_equal
@@ -50,15 +53,70 @@ def test_laziness():
 
 
 def test_predict_meta_override():
-    pass
+    X = pd.DataFrame({"c_0": [1, 2, 3, 4]})
+    y = np.array([1, 2, 3, 4])
+
+    base = CategoricalNB()
+    base.fit(pd.DataFrame(X), y)
+
+    dd_X = dd.from_pandas(X, npartitions=2)
+    dd_X._meta = pd.DataFrame({"c_0": [5]})
+
+    # Failure when not proving predict_meta
+    # because of value dependent model
+    wrap = ParallelPostFit(base)
+    with pytest.raises(ValueError):
+        wrap.predict(dd_X)
+
+    # Success when providing meta over-ride
+    wrap = ParallelPostFit(base, predict_meta=np.array([1]))
+    result = wrap.predict(dd_X)
+    expected = base.predict(X)
+    assert_eq_ar(result, expected)
 
 
-def test_score_meta_override():
-    pass
+def test_predict_proba_meta_override():
+    X = pd.DataFrame({"c_0": [1, 2, 3, 4]})
+    y = np.array([1, 2, 3, 4])
+
+    base = CategoricalNB()
+    base.fit(pd.DataFrame(X), y)
+
+    dd_X = dd.from_pandas(X, npartitions=2)
+    dd_X._meta = pd.DataFrame({"c_0": [5]})
+
+    # Failure when not proving predict_proba_meta
+    # because of value dependent model
+    wrap = ParallelPostFit(base)
+    with pytest.raises(ValueError):
+        wrap.predict_proba(dd_X)
+
+    # Success when providing meta over-ride
+    wrap = ParallelPostFit(base, predict_proba_meta=np.array([[0.0, 0.1, 0.8, 0.1]]))
+    result = wrap.predict_proba(dd_X)
+    expected = base.predict_proba(X)
+    assert_eq_ar(result, expected)
 
 
-def transform_meta_overide():
-    pass
+def test_transform_meta_override():
+    X = pd.DataFrame({"cat_s": ["a", "b", "c", "d"]})
+    dd_X = dd.from_pandas(X, npartitions=2)
+
+    base = OneHotEncoder(sparse=False)
+    base.fit(pd.DataFrame(X))
+
+    # Failure when not proving transform_meta
+    # because of value dependent model
+    wrap = ParallelPostFit(base)
+    with pytest.raises(ValueError):
+        wrap.transform(dd_X)
+
+    wrap = ParallelPostFit(
+        base, transform_meta=np.array([[0, 0, 0, 0]], dtype=np.float64)
+    )
+    result = wrap.transform(dd_X)
+    expected = base.transform(X)
+    assert_eq_ar(result, expected)
 
 
 def test_predict_correct_output_dtype():
