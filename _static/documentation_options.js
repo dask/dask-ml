89952457546f2d9ed920faa694f16f3dfb77d725
jrbourbeau <jrbourbeau@users.noreply.github.com>
2021-11-18 22:23:00 +0000

    Deploying to gh-pages from  @ cf2410042972b944569c7914352b7c9c5132d9e5 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.10.18',
+    VERSION: '2021.11.17',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
