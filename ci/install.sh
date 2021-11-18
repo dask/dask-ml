cf2410042972b944569c7914352b7c9c5132d9e5
James Bourbeau <jrbourbeau@users.noreply.github.com>
2021-11-18 16:19:11 -0600

    Add upstream CI build (#882)

--- ci/install.sh
+++ ci/install.sh
@@ -1,3 +1,21 @@
+
+# Optionally, install development versions of dependenies
+if [[ ${UPSTREAM_DEV} ]]; then
+    # FIXME https://github.com/mamba-org/mamba/issues/412
+    # mamba uninstall --force dask distributed scikit-learn
+    conda uninstall --force dask distributed scikit-learn
+
+    python -m pip install --no-deps --pre \
+        -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
+        scikit-learn
+
+    python -m pip install \
+        --upgrade \
+        git+https://github.com/dask/dask \
+        git+https://github.com/dask/distributed
+fi
+
+# Install dask-ml
 python -m pip install --quiet --no-deps -e .
 
 echo mamba list
