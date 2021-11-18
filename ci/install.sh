2cba6c044b8b5e65c4ad4c7e44635381b80e8c74
James Bourbeau <jrbourbeau@gmail.com>
2021-11-18 15:22:17 -0600

    Update upstream packages

--- ci/install.sh
+++ ci/install.sh
@@ -2,18 +2,15 @@
 # Optionally, install development versions of dependenies
 if [[ ${UPSTREAM_DEV} ]]; then
     # FIXME https://github.com/mamba-org/mamba/issues/412
-    # mamba uninstall --force numpy pandas scikit-learn
-    conda uninstall --force numpy pandas scikit-learn
+    # mamba uninstall --force dask distributed scikit-learn
+    conda uninstall --force dask distributed scikit-learn
 
     python -m pip install --no-deps --pre \
         -i https://pypi.anaconda.org/scipy-wheels-nightly/simple \
-        numpy \
-        pandas \
         scikit-learn
 
     python -m pip install \
         --upgrade \
-        git+https://github.com/pydata/sparse \
         git+https://github.com/dask/dask \
         git+https://github.com/dask/distributed
 fi
