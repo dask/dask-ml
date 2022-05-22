a5b96edf863e3540be83c8321a9e2aa37dc91b4a
Chiara Marmo <cmarmo@users.noreply.github.com>
2022-05-22 02:18:20 -1000

    Fix precomputed kernel test with _safe_tags. (#921)

--- dask_ml/model_selection/_search.py
+++ dask_ml/model_selection/_search.py
@@ -31,7 +31,7 @@ from sklearn.model_selection._split import (
     _CVIterableWrapper,
 )
 from sklearn.pipeline import FeatureUnion, Pipeline
-from sklearn.utils.estimator_checks import _is_pairwise_metric
+from sklearn.utils._tags import _safe_tags
 from sklearn.utils.metaestimators import if_delegate_has_method
 from sklearn.utils.multiclass import type_of_target
 from sklearn.utils.validation import _num_samples, check_is_fitted
@@ -203,7 +203,7 @@ def build_cv_graph(
     X, y, groups = to_indexable(X, y, groups)
     cv = check_cv(cv, y, is_classifier(estimator))
     # "pairwise" estimators require a different graph for CV splitting
-    is_pairwise = _is_pairwise_metric(estimator)
+    is_pairwise = _safe_tags(estimator, "pairwise")
 
     dsk = {}
     X_name, y_name, groups_name = to_keys(dsk, X, y, groups)
