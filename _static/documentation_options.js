731d262f5a58ba9f6a42c9aa4939fc50bd25ec2e
jacobtomlinson <jacobtomlinson@users.noreply.github.com>
2022-06-06 15:39:52 +0000

    Deploying to gh-pages from  @ 1eee06113c7d53623bc201a6fdd26f5e91a88b49 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2021.1.23',
+    VERSION: '2022.5.28',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
