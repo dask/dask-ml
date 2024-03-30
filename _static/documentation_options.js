25a15505e85ac1be4c7d30a41f90affbd4dececb
TomAugspurger <tom.augspurger88@gmail.com>
2024-03-30 18:23:48 +0000

    Deploying to gh-pages from  @ 9424993a27903e62c03e46e53ed564a3237f8bae 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '2023.3.25',
+    VERSION: '2024.3.21',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
